588ad77733cc0b10219cc59f90af748c10bcb2b7
Phil Hartley <philh725@gmail.com>
2018-10-12 05:14:27 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -4,4 +4,10 @@ const main = document.getElementById('main');
  
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
-});  
+});
+
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
